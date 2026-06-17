b4fc9c337723881b38975c89920fbcdb3082c515
Shedowe <shedowe@users.noreply.github.com>
2026-06-17 09:40:02 +0200

    fix: complete WireGuard route and key cleanup

--- eblocker-icapserver/src/main/java/org/eblocker/server/http/server/EblockerHttpsServer.java
+++ eblocker-icapserver/src/main/java/org/eblocker/server/http/server/EblockerHttpsServer.java
@@ -2077,6 +2077,10 @@ public class EblockerHttpsServer implements Preprocessor {
                 .action("getConfigurations", HttpMethod.GET)
                 .name("dashboard.vpn.mobile.configurations.get");
         server
+                .uri("/api/dashboard/wireguard/configurations/{deviceId}", wireGuardMobileController)
+                .action("disableDevice", HttpMethod.DELETE)
+                .name("dashboard.vpn.mobile.configurations.delete");
+        server
                 .uri("/api/dashboard/wireguard/configurations/generateDownloadUrl/{deviceId}/{deviceType}", wireGuardMobileController)
                 .action("generateDownloadUrl", HttpMethod.GET)
                 .name("dashboard.vpn.mobile.configurations.generateDownloadUrl.get");
@@ -2085,6 +2089,14 @@ public class EblockerHttpsServer implements Preprocessor {
                 .action("downloadClientConf", HttpMethod.GET)
                 .name("dashboard.vpn.mobile.configurations.downloadClientConf.get")
                 .noSerialization();
+        server
+                .uri("/api/dashboard/wireguard/test", mobileConnectionCheckController)
+                .action("start", HttpMethod.POST)
+                .name("dashboard.vpn.test.start");
+        server
+                .uri("/api/dashboard/wireguard/test", mobileConnectionCheckController)
+                .action("getStatus", HttpMethod.GET)
+                .name("dashboard.vpn.test.status");
         server
                 .uri("/api/dashboard/customdomainfilter/{userId}", customDomainFilterConfigController)
                 .action("getFilter", HttpMethod.GET)
